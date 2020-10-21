f04d6f4444c33290bccb3c1779cbf6ab8ec8af8e
radiogron <arseny@i-am.studio>
2020-10-21 23:16:22 +0300

    Removed unused, fixed identation

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,30 @@
 const gulp = require('gulp');
 const sassToCss = require('gulp-sass');
 const sassGlobbing = require('gulp-sass-glob');
-const pug = require('gulp-pug');
-const plumber = require('gulp-plumber');
-const imagemin = require('gulp-imagemin');
-var flatten = require('gulp-flatten');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const { VueLoaderPlugin } = require('vue-loader');
 //dependencies for build END
 
 //settings
-var sass = 'sass/';
-var js = 'js/';
-var dest = 'dist/';
-var test = 'test/build/';
+
 //settings END
 async function style() {
-	return gulp
-		.src( sass + 'fa-kit.sass' )
-		.pipe( sassGlobbing() )
-		.pipe( sassToCss().on( 'error', sassToCss.logError ) )
-		.pipe( gulp.dest( dest ) );
+    return gulp
+        .src('./sass/fa-kit.sass')
+        .pipe(sassGlobbing())
+        .pipe(sassToCss().on('error', sassToCss.logError))
+        .pipe(gulp.dest('./dist'));
 }
-
-function markup() {
-	return gulp
-		.src(test+'pug/**/*.pug')
-		.pipe( plumber() )
-		.pipe( pug( { pretty: true } ) )
-		.pipe( gulp.dest( test ) );
-}
-//build project END
 
 function watch() {
-	gulp.watch(sass+'**/*.sass', style);
-	gulp.watch(test+'**/*.pug', markup);
+    gulp.watch('./sass/**/*.sass', style);
 }
 
 exports.style = style
-exports.markup = markup
 exports.watch = watch
 
-const build = gulp.series(gulp.parallel(style, markup));
+const build = gulp.parallel(style);
 const workon = gulp.series(build, watch);
 
 exports.build = build
